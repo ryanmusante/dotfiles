bc5d0409c61b16ddaf4f66c55e6b59bc066cb748
Ryan Musante <26808646+ryanmusante@users.noreply.github.com>
2023-08-18 17:13:56 -0700

    Delete linux-tkg.sh

--- linux-tkg.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#sudo rm -rf ~/community-patches
-sudo rm -rf /home/ryan/.cache/linux-tkg
-cd /home/ryan/.cache
-git clone https://github.com/Frogging-Family/linux-tkg.git
-cd /home/ryan/.cache/linux-tkg
-makepkg -sirc --noconfirm
